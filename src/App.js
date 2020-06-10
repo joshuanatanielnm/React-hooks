5c61ab798f2c121b85fb90f383ccceb6f48376e4
Joshua Nathaniel M <clsjoshua@gmail.com>
2020-06-11 00:12:48 +0700

    update color

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'Style/Style.scss'
+
 import Calculator from 'Hooks-Practice/Calculator'
 import ComponentState from 'Hooks-Practice/ComponentState'
 import React from 'react'
--- src/Hooks-Practice/Calculator/index.jsx
+++ src/Hooks-Practice/Calculator/index.jsx
@@ -1,6 +1,4 @@
-import 'Style/Style.scss'
-
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Calculator() {
   const [value, setValue] = useState('')
@@ -8,34 +6,37 @@ export default function Calculator() {
   const Operator = ['+', '-', '*', '/', '=']
 
   return (
-    <div className='flex justify-center text-3xl'>
-      <div className='w-64 mt-20'>
-        <input
-          type='text'
-          className='border-solid border-2 border-gray-600 w-full h-20 rounded-lg p-4'
-          readOnly
-          value={value}
-        />
-        <div className='grid grid-cols-4 grid-rows-4 text-center gap-2 mt-4'>
-        {
-          numBtns.map(numb => <button
-            className='bg-gray-200 rounded-lg cursor-pointer hover:bg-gray-300'
-            onClick={(e) => numb === 'CR' ? setValue(''):setValue(value + e.target.value)}
-            value={numb} key={numb}
-          >
-          {numb}
-          </button>)
-        }
-        {
-          Operator.map(oprt => <button
-            className='bg-blue-200 rounded-lg cursor-pointer hover:bg-blue-300 col-span-1'
-            value={oprt} key={oprt}
-            onClick={(e) => oprt === '=' ? setValue(String(eval(value))) :setValue(value + e.target.value)}
-          >
-          {oprt}
-          </button> )
-        }
+    <div>
+    <h1 className="text-yellow-400 flex justify-center text-3xl mt-20">Simple Calculator</h1>
+      <div className='flex justify-center text-3xl'>
+        <div className='w-64 mt-5  border-2 p-5 rounded-lg'>
+          <input
+            type='text'
+            className='border-solid border-2 border-gray-600 w-full h-20 rounded-lg p-4'
+            readOnly
+            value={value}
+          />
+          <div className='grid grid-cols-4 grid-rows-4 text-center gap-2 mt-4'>
+          {
+            numBtns.map(numb => <button
+              className='bg-gray-200 rounded-lg cursor-pointer hover:bg-gray-300'
+              onClick={(e) => numb === 'CR' ? setValue(''):setValue(value + e.target.value)}
+              value={numb} key={numb}
+            >
+            {numb}
+            </button>)
+          }
+          {
+            Operator.map(oprt => <button
+              className='bg-blue-300 rounded-lg cursor-pointer hover:bg-blue-400 col-span-1'
+              value={oprt} key={oprt}
+              onClick={(e) => oprt === '=' ? setValue(String(eval(value))) :setValue(value + e.target.value)}
+            >
+            {oprt}
+            </button> )
+          }
 
+          </div>
         </div>
       </div>
     </div>
